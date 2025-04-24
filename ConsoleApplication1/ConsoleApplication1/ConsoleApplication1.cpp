df69d10e889422769ed47b3573ead54380ea6107
Wojciech Książek <wojtekksiazekk@gmail.com>
2025-04-24 14:11:24 +0200

    Kolejne zmiany

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -8,6 +8,7 @@ int main()
     std::cout << "Hello World!\n";
     std::cout << "Hello World!\n";
     std::cout << "Hello World!\n";
+    std::cout << "test2222!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
